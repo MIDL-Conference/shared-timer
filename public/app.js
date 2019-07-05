5891057ed7a7d7d1fec6ed72bf134580d6131ac0
Fernando Perez-Garcia <fepegar@gmail.com>
2019-07-05 18:31:17 +0100

    Add colors and radio buttons

--- public/app.js
+++ public/app.js
@@ -3,7 +3,12 @@ var secondsLeft;
 var urlParams;
 var isAdmin;
 var interval;
-
+var adminControls = document.querySelector("#adminControls");
+var radioButtons = document.querySelector("#radioButtons");
+var clock = document.querySelector("#clockdiv");
+var orangeThreshold;
+var redThreshold = 0;
+var fixedDuration = false;
 
 function domLoaded() {
   try {
@@ -19,7 +24,23 @@ function domLoaded() {
   }
 }
 
-function settimerseconds(seconds) {
+function settimerseconds() {
+  // https://stackoverflow.com/a/15839451/3956024
+  var minutes;
+  var seconds;
+  if (fixedDuration) {
+    if (urlParams.has("minutes")) {
+      minutes = parseInt(urlParams.get("minutes"));
+      seconds = 60 * minutes;
+    } else if (urlParams.has("seconds")) {
+      seconds = parseInt(urlParams.get("seconds"));
+    }
+  } else {
+    var element = document.querySelector('input[name="talkDuration"]:checked')
+    minutes = parseInt(element.value);
+    seconds = 60 * minutes;
+  }
+
   var documentObject = {};
   var docRef = db.collection("common").doc("h2eg1Gl589Z1PDRZoVrp")
   documentObject.deadline = Date.now() / 1000 + seconds;
@@ -29,9 +50,9 @@ function settimerseconds(seconds) {
 function checkTime() {
   var docRef = db.collection("common").doc("h2eg1Gl589Z1PDRZoVrp")
   docRef.get().then(function (thisDoc) {
-    let message;
     let minutes;
     let seconds;
+    let timeString;
 
     deadline = thisDoc.data().deadline;
 
@@ -39,29 +60,49 @@ function checkTime() {
     secondsLeft = Math.floor(deadline - now);
 
     if (secondsLeft < 1) {
-      message = "TIME'S UP";
-      minutes = 0;
-      seconds = 0;
+      minutes = -Math.ceil(secondsLeft / 60);
+      seconds = -Math.ceil(secondsLeft % 60);
+      timeString = `-${minutes}:${zeroFill(seconds, 2)}`;
+      document.body.style.background = "crimson";
     } else {
-      message = "Counting";
       minutes = Math.floor(secondsLeft / 60);
       seconds = Math.floor(secondsLeft % 60);
+      timeString = `${minutes}:${zeroFill(seconds, 2)}`;
+      if (secondsLeft < orangeThreshold) {
+        document.body.style.background = "orange";
+      } else {
+        document.body.style.background = "black";
+      }
     }
-
-    document.getElementById("bottommsg").innerHTML = message;
-    document.getElementById("minute").innerHTML = minutes;
-    document.getElementById("second").innerHTML = seconds;
-
+    clock.innerHTML = timeString;
   });
 }
 
+function zeroFill(number, width) {
+  // https://stackoverflow.com/a/1267338/3956024
+  width -= number.toString().length;
+  if (width > 0) {
+    return new Array(width + (/\./.test(number) ? 2 : 1)).join('0') + number;
+  }
+  return number + ""; // always return a string
+}
+
 urlParams = new URLSearchParams(window.location.search);
-console.log(urlParams.toString());
-
 isAdmin = urlParams.get("admin") == "true";
-
 if (!isAdmin) {
-  document.getElementById("resetButton").style.visibility = "hidden";
+  adminControls.style.visibility = "hidden";
+  clock.style.fontSize = "70vmin";
+}
+
+if (urlParams.has("minutes") || urlParams.has("seconds")) {
+  radioButtons.style.visibility = "hidden";
+  fixedDuration = true;
+}
+
+if (urlParams.has("debug")) {
+  orangeThreshold = 5;
+} else {
+  orangeThreshold = 60;
 }
 
 document.addEventListener('DOMContentLoaded', domLoaded);
