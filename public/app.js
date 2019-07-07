2414ac85966356951bbd9ca929a4c07b56f3675a
Tom Vercauteren <t.vercauteren@ucl.ac.uk>
2019-07-07 11:12:48 +0100

    add an option to fecth from local storage rather than from the server

--- public/app.js
+++ public/app.js
@@ -2,25 +2,30 @@ var db;
 var secondsLeft;
 var urlParams;
 var isAdmin;
+var isLocal;
 var interval;
 var adminControls = document.querySelector("#adminControls");
 var radioButtons = document.querySelector("#radioButtons");
 var clock = document.querySelector("#clockdiv");
+var msg = document.querySelector("#msgdiv");
 var orangeThreshold;
 var redThreshold = 0;
 var fixedDuration = false;
 
 function domLoaded() {
-  try {
-    let app = firebase.app();
-    db = firebase.firestore();
-
-    let features = ['auth', 'database', 'messaging', 'storage'].filter(feature => typeof app[feature] ===
-      'function');
-
-  } catch (e) {
-    console.error(e);
-    document.getElementById('load').innerHTML = 'Error loading the Firebase SDK, check the console.';
+  if (!isLocal) {
+    try {
+      let app = firebase.app();
+      db = firebase.firestore();
+  
+      let features = ['auth', 'database', 'messaging', 'storage'].filter(feature => typeof app[feature] ===
+        'function');
+  
+    } catch (e) {
+      console.error(e);
+      msg.innerHTML = 'Error loading the Firebase SDK, check the console.';
+      isLocal = true;
+    }
   }
 }
 
@@ -42,40 +47,60 @@ function settimerseconds() {
   }
 
   var documentObject = {};
-  var docRef = db.collection("common").doc("h2eg1Gl589Z1PDRZoVrp")
   documentObject.deadline = Date.now() / 1000 + seconds;
-  docRef.update(documentObject);
+  // set in local storage anyway
+  localStorage.setItem("deadline",documentObject.deadline);
+  if (!isLocal) {
+    var docRef = db.collection("common").doc("h2eg1Gl589Z1PDRZoVrp")
+    docRef.update(documentObject);
+  }
+}
+
+function setDisplayTime(deadline) {
+  let minutes;
+  let seconds;
+  let timeString;
+
+  let now = Date.now() / 1000;
+  secondsLeft = Math.floor(deadline - now);
+
+  if (secondsLeft < 1) {
+    minutes = -Math.ceil(secondsLeft / 60);
+    seconds = -Math.ceil(secondsLeft % 60);
+    timeString = `-${minutes}:${zeroFill(seconds, 2)}`;
+    document.body.style.background = "crimson";
+  } else {
+    minutes = Math.floor(secondsLeft / 60);
+    seconds = Math.floor(secondsLeft % 60);
+    timeString = `${minutes}:${zeroFill(seconds, 2)}`;
+    if (secondsLeft < orangeThreshold) {
+      document.body.style.background = "orange";
+    } else {
+      document.body.style.background = "black";
+    }
+  }
+  clock.innerHTML = timeString;
 }
 
 function checkTime() {
-  var docRef = db.collection("common").doc("h2eg1Gl589Z1PDRZoVrp")
-  docRef.get().then(function (thisDoc) {
-    let minutes;
-    let seconds;
-    let timeString;
-
-    deadline = thisDoc.data().deadline;
-
-    let now = Date.now() / 1000;
-    secondsLeft = Math.floor(deadline - now);
-
-    if (secondsLeft < 1) {
-      minutes = -Math.ceil(secondsLeft / 60);
-      seconds = -Math.ceil(secondsLeft % 60);
-      timeString = `-${minutes}:${zeroFill(seconds, 2)}`;
-      document.body.style.background = "crimson";
-    } else {
-      minutes = Math.floor(secondsLeft / 60);
-      seconds = Math.floor(secondsLeft % 60);
-      timeString = `${minutes}:${zeroFill(seconds, 2)}`;
-      if (secondsLeft < orangeThreshold) {
-        document.body.style.background = "orange";
-      } else {
-        document.body.style.background = "black";
-      }
-    }
-    clock.innerHTML = timeString;
-  });
+  // fetch from local storage anyway
+  deadline = 0;//parseInt(localStorage.getItem("deadline"));
+  if (!isLocal) {
+      var docRef = db.collection("common").doc("h2eg1Gl589Z1PDRZoVrp")
+      docRef.get().then(function (thisDoc) {
+          deadline = thisDoc.data().deadline;
+	  setDisplayTime(deadline);
+      }).catch(function(error) {
+	  console.error(error);
+	  msg.innerHTML = "Error: using local deadline";
+          deadline = parseInt(localStorage.getItem("deadline"));
+          setDisplayTime(deadline);
+      });
+  }
+  else {
+    deadline = parseInt(localStorage.getItem("deadline"));
+    setDisplayTime(deadline);
+  }
 }
 
 function zeroFill(number, width) {
@@ -92,6 +117,11 @@ isAdmin = urlParams.get("admin") == "true";
 if (!isAdmin) {
   adminControls.style.visibility = "hidden";
   clock.style.fontSize = "70vmin";
+}
+
+isLocal = urlParams.get("local") == "true";
+if (isLocal) {
+  msg.innerHTML = msg.innerHTML + "Using local deadline";
 }
 
 if (urlParams.has("minutes") || urlParams.has("seconds")) {
